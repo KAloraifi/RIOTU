c340af9df807efb2147b5f6b461dd7c5f45240ad
MosabDev <Mosab@DiskSol.com>
2017-12-23 16:35:47 +0300

    Adding Pages and Information for all tables and admin dashboard page with stats

---
+++ WebContent/dashboard/news.php
@@ -0,0 +1,88 @@
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="widget-box">
+          <div class="widget-title">
+            </div>
+          <div class="widget-content nopadding">
+              
+              
+              
+              <?php
+
+include 'include/dbconfig2.php';
+
+				$sql = "SELECT * FROM news ";
+				$result = $conn->query($sql);
+
+
+				echo '<table class="table table-bordered data-table">';
+				echo '<thead>
+									<tr>
+										<th>No.</th>
+										<th>Title</th>
+										<th>Short Description</th>
+										<th>Long Description</th>
+										<th>Date</th>
+										<th>Pictures</th>
+                                        <th> Action </th>
+
+                                        
+									</tr>
+                                    
+								</thead>';
+				echo'<tbody id="InvoiceTableBody">';
+				$number_item=0;
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+						$number_item+=1;
+                        $title=$row["title"];
+                        $shortDescription=$row["shortDescription"];
+                        $longDescription=$row["longDescription"];
+                        $date=$row["date"];
+                        $picture1=$row["picture1"];
+                        $picture2=$row["picture2"];
+                        $picture3=$row["picture3"];
+                        $picture4=$row["picture4"];
+						
+						
+                        //put a condition to check if the news date is less than 2 days (NEW) is soon or middle or far away
+   
+                        $date1=date_create(date("Y-m-d"));
+                        $date2=date_create($date);
+                        $diff=date_diff($date1,$date2);                        
+                        
+                        $daysRemaining = $diff->format('%a');
+                        
+						if ($daysRemaining >= -2)
+						echo '<tr class="success">';
+                        
+                        
+						else echo '<tr>';
+                        
+						echo '
+							<td>'.$number_item.'</td>
+							<td>'.$title.'</td>
+							<td>'.$shortDescription.'</td>
+							<td>'.$longDescription.'</td>
+							<td>'.$date.'</td>
+							<td>'.$picture1.' '.$picture2.' '.$picture3.' '.$picture4.'</td>
+                            <td> 
+                                <a href="#" class="btn btn-primary btn-mini">Edit</a>
+                                <a href="#" class="btn btn-danger btn-mini">Delete</a>
+                            </td>
+						   </tr>';
+
+					}
+				}
+				echo'</tbody>';
+				echo '</table>';
+				echo '</div>';
+				?>
+              
+              
+        </div>
+       
+    </div>
+  </div>
+</div>
---
+++ WebContent/dashboardAdmin.php
@@ -0,0 +1,191 @@
+﻿
+        <!-- /navbar -->
+        <div class="wrapper">
+            <?php
+            include 'include/dbconfig2.php';
+            
+            ?>
+            <div class="container">
+                <div class="row">
+                    <div class="span1">
+                    </div>
+                    
+                    <!--/.span3-->
+                    <div class="span10">
+                        <div class="content">
+                            <div class="btn-controls">
+                                <div class="btn-box-row row-fluid">
+                                    <a href="pubDashboard.php" class="btn-box big span4"><i class="fa fa-book" aria-hidden="true"></i><b>
+                                        
+                                          <?php  
+                                    $sql = "SELECT COUNT(title) FROM publication";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                    echo $row['COUNT(title)'];
+                                    
+                                    ?>
+                                        </b>
+                                        <p class="text-muted">
+                                            Publications</p>
+                                    </a>
+                                    
+                                    
+                                    
+                                    <a href="userDashboard.php" class="btn-box big span4"><i class="fa fa-users" aria-hidden="true"></i><b>
+                                    
+                                    <?php  
+                                    $sql = "SELECT COUNT(username) FROM users";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                    echo $row['COUNT(username)'];
+                                    
+                                    ?>
+                                    </b>
+                                        <p class="text-muted">
+                                            New Users</p>
+                                    </a>
+                                    
+                                    
+                                    
+                                    
+                                    <a href="projDashboard.php" class="btn-box big span4"><i class="fa fa-money" aria-hidden="true"></i><b>
+                                        
+                                         <?php  
+                                    $sql = "SELECT SUM(totalBudget) FROM projects";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                    echo $row['SUM(totalBudget)'] . " SAR";
+                                    
+                                    ?>
+                                        
+                                        </b>
+                                        <p class="text-muted">
+                                            Projects Budget</p>
+                                    </a>
+                                </div>
+                                <div class="btn-box-row row-fluid">
+                                    <div class="span8">
+                                        <div class="row-fluid">
+                                            <div class="span12">
+                                                <a href="userDashboard.php" class="btn-box small span4"><i class="fa fa-users" aria-hidden="true"></i><b>Users</b>
+                                                </a><a href="pubDashboard.php" class="btn-box small span4"><i class="fa fa-book" aria-hidden="true"></i><b>Publications</b>
+                                                </a><a href="dashboard/conf.php" class="btn-box small span4"><i class="fa fa-commenting" aria-hidden="true"></i><b>Conferences</b>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="row-fluid">
+                                            <div class="span12">
+                                                <a href="projDashboard.php" class="btn-box small span4"><i class="fa fa-tasks" aria-hidden="true"></i><b>Projects</b>
+                                                </a><a href="newsDashboard.php" class="btn-box small span4"><i class="fa fa-newspaper-o" aria-hidden="true"></i><b>News</b>
+                                                </a><a href="index.php" class="btn-box small span4"><i class="fa fa-home" aria-hidden="true"></i><b>Main Page</b> </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <ul class="widget widget-usage unstyled span4">
+                                        <li>
+                                            <p>
+                                                <strong>Finished Projects</strong> <span class="pull-right small muted">
+                                                
+                                                
+                                                
+                                         <?php  
+                                    $sql = "SELECT COUNT(name) FROM projects";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                    //echo $row['COUNT(name)'];
+                                                
+                                    $dateNow=date("Y-m-d");
+                                    $sql1 = "SELECT COUNT(name) FROM projects WHERE endDate <=' $dateNow '";
+                                    $result1 = $conn->query($sql1);
+                                    $row1=$result1->fetch_assoc();
+                                    //echo $row1['COUNT(name)'];
+                                                
+                                                $finishedProjects = ($row1['COUNT(name)']/$row['COUNT(name)'])*100 . "%";
+                                                echo $finishedProjects;
+                                    ?>
+                                                
+                                                
+                                                </span>
+                                            </p>
+                                            <div class="progress tight">
+                                                <div class="bar" style="width: <?php echo $finishedProjects ?>;">
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <p>
+                                                <strong>Passed Conferences</strong> <span class="pull-right small muted">
+                                                
+                                                
+                                                 <?php  
+                                    $sql = "SELECT COUNT(name) FROM conferences";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                    //echo $row['COUNT(name)'];
+                                                
+                                    $dateNow=date("Y-m-d");
+                                    $sql1 = "SELECT COUNT(name) FROM conferences WHERE endDate <=' $dateNow '";
+                                    $result1 = $conn->query($sql1);
+                                    $row1=$result1->fetch_assoc();
+                                    //echo $row1['COUNT(name)'];
+                                                
+                                                $finishedConfs = ($row1['COUNT(name)']/$row['COUNT(name)'])*100 . "%";
+                                                echo $finishedConfs;
+                                    ?>
+                                                
+                                                
+                                                
+                                                
+                                                </span>
+                                            </p>
+                                            <div class="progress tight">
+                                                <div class="bar bar-success" style="width:<?php   echo $finishedConfs; ?>">
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <p>
+                                                <strong>Today's new uers</strong> <span class="pull-right small muted">
+                                                
+                                                
+                                                <?php  
+                                    $sql = "SELECT COUNT(username) FROM users";
+                                    $result = $conn->query($sql);
+                                    $row=$result->fetch_assoc();
+                                                
+                                    $dateNow=date("Y-m-d");
+                                    $sql1 = "SELECT COUNT(username) FROM users WHERE joinDate <=' $dateNow '";
+                                    $result1 = $conn->query($sql1);
+                                    $row1=$result1->fetch_assoc();
+                                    //echo $row1['COUNT(username)'];
+                                                
+                                                if ($row['COUNT(username)'] != 0){
+                                                $newUsers =(int) (($row1['COUNT(username)']/$row['COUNT(username)'])*100) . "%";
+                                                echo $newUsers;
+                                                } else $newUsers = 0;
+                                    ?>
+                                                
+                                                
+                                                </span>
+                                            </p>
+                                            <div class="progress tight">
+                                                <div class="bar bar-warning" style="width:<?php   echo $newUsers; ?>" >
+                                                </div>
+                                            </div>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+                            <!--/#btn-controls-->
+                            
+                        <!--/.content-->
+                    </div>
+                    </div>
+                    <!--/.span9-->
+                </div>
+            </div>
+            <!--/.container-->
+        </div>
+        <!--/.wrapper-->
+        
